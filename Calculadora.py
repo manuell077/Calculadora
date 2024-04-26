19ec43dcd4ec5c75bc6b9f1adff4890b388b01f1
unknown <manuell077>
2024-04-25 20:42:39 -0500

    Añadi que la ventana no se puede modificar su tamaño,agregue icono de aplicacion y le puse medidas a la ventana

--- Calculadora.py
+++ Calculadora.py
@@ -13,5 +13,19 @@ from tkinter import *  #libreria para la interfaz grafica
 
 raiz = Tk() #en este caso como vamos a crear la raiz la llamamos asi 
 
+raiz.title("Calculadora") #Para ponerle un titulo a nuestra venta utilizamos el metodo title 
+
+raiz.resizable(False,False)#este metodo sirve para que podamos agrandar o no la ventana con un true o un false 
+
+
+raiz.iconbitmap("imagenes/calculadora.ico")
+
+raiz.geometry("300x500")
+# si ponemos algo fuera de este metodo no va a funcionar
 raiz.mainloop() #y ponemos este metodo para que se ejecute
 #el metodo mainloop  nos sirve ya que para que se nos muestre la ventana tenemos que hacer como un bucle de manera infinita y para eso esta este metodo 
+
+
+
+
+
