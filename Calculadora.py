03fb2781330d558b043e6335b9e1598c1dc54427
unknown <manuell077>
2024-04-25 20:06:34 -0500

    creacion de la raiz de la interfaz

---
+++ Calculadora.py
@@ -0,0 +1,17 @@
+from tkinter import *  #libreria para la interfaz grafica
+
+'''la estructura de tkinter viene de la siguiente forma
+
+-raiz
+-frame
+-wigets
+
+
+''' 
+
+#declaramos una variable con la clase tkinter
+
+raiz = Tk() #en este caso como vamos a crear la raiz la llamamos asi 
+
+raiz.mainloop() #y ponemos este metodo para que se ejecute
+#el metodo mainloop  nos sirve ya que para que se nos muestre la ventana tenemos que hacer como un bucle de manera infinita y para eso esta este metodo 
