668c0d4d001d4bff85e0f3f20eb3a265036956f8
unknown <manuell077>
2024-04-26 20:59:13 -0500

    Agregue un color de fondo , y agregue el frame con su respectivo tamaño

--- Calculadora.py
+++ Calculadora.py
@@ -18,9 +18,18 @@ raiz.title("Calculadora") #Para ponerle un titulo a nuestra venta utilizamos el 
 raiz.resizable(False,False)#este metodo sirve para que podamos agrandar o no la ventana con un true o un false 
 
 
-raiz.iconbitmap("imagenes/calculadora.ico")
+raiz.iconbitmap("imagenes/calculadora.ico")#ponerle un icono a la aplicacion
 
-raiz.geometry("300x500")
+
+
+frame = Frame() #para crear un frame llamamos a su clase 
+frame.config(width="400",height="500") #para ponerle un tamaño a la ventana hacemos lo siguiente , SOLO al frame
+
+frame.pack() #tenemos que empaquetar el frame dentro de la raiz por lo cual utilizamos el siguiente metodo 
+frame.config(bg="grey") #para cambiar el color de fondo en este caso del frame 
+frame.config(bd=10)
+frame.config(relief=SUNKEN)
+
 # si ponemos algo fuera de este metodo no va a funcionar
 raiz.mainloop() #y ponemos este metodo para que se ejecute
 #el metodo mainloop  nos sirve ya que para que se nos muestre la ventana tenemos que hacer como un bucle de manera infinita y para eso esta este metodo 
