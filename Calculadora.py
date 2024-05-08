b5c0113601e09f70bd65794aeeff8c207de3c89d
unknown <manuell077>
2024-05-07 20:03:17 -0500

    Le añadi el resultado que va a venir por defecto

--- Calculadora.py
+++ Calculadora.py
@@ -1,5 +1,6 @@
 from tkinter import *  #libreria para la interfaz grafica
 
+resultado = 0
 '''la estructura de tkinter viene de la siguiente forma
 
 -raiz
@@ -27,6 +28,12 @@ frame.config(width="400",height="600") #para ponerle un tamaño a la ventana hac
 
 frame.pack() #tenemos que empaquetar el frame dentro de la raiz por lo cual utilizamos el siguiente metodo 
 frame.config(bg="grey") #para cambiar el color de fondo en este caso del frame 
+
+
+resultadoLabel = Label(frame,text=resultado,font=("Arial",60))#Aqui es un label de tipo texto que va a mostrar el resultado que tenemos almacenado en una variable llamada "resultado" ademas le decimos el tipo de fuente y el tamaño con el parametro font("nombreFuente",tamaño)
+#la estructura de un label es label(contenedorPadre,opcion) hay diferentes opciones
+resultadoLabel.config(bg="grey")#cambiamos el color de fondo del label 
+resultadoLabel.place(x=330 , y= 50)#aqui mediante el metodo place podemos ubicar el texto dentro del contenedor padre con unas cordenadas "x" "y" 
 
 
 # si ponemos algo fuera de este metodo no va a funcionar
