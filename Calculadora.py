cc6b0e8966ae2f833db8ba204434ad327980bd43
unknown <manuell077>
2024-05-29 22:38:58 -0500

    Estructura basica del codigo

--- Calculadora.py
+++ Calculadora.py
@@ -1,28 +1,28 @@
 from tkinter import *
 
 
-#funciones para poner atributos por defecto
-def ButtonAtributos(contenedor,texto,x,y):#atributos de button 
-    
-    nombre = Button(contenedor,text=texto,height=8,width=10)
-    nombre.grid(row=x,column=y , sticky="nsew")
+#funcion para mandar numeros a la pantalla 
 
-    return nombre
 
-def LabelAtributos(contenedor,texto,x,y):
+def CaracteresPantalla(seleccion):
     
-    label = Label(contenedor,textvariable=texto,justify="right",height=3)
-    label.config(font=("ARIAL",15))#tamaño de fuente recibe como parametros el nombre de la fuente y el tamaño 
-    label.grid(row=x,column=y)
+    if resultado.get() == "0":
+        
+        resultado.set("")
+        resultado.set(seleccion)
+        
+    else:
+        
+        resultado.set(resultado.get()+seleccion)
     
 
-    
-    return label
+
+
 
 
 raiz = Tk()#raiz del proyecto
 raiz.title("Calculadora")
-raiz.resizable(False,False)
+
 
 raiz.iconbitmap("imagenes/calculadora.ico")
 
@@ -30,36 +30,60 @@ frame = Frame(raiz,width=40)#frame del proyecto
 frame.pack()#empaquetamos la calculadora
 
 
-#variables que van a  enviar una notificacion cuando sean modificadas
-resultado = StringVar()
-resultado.set("0") #Que el texto por defecto sea 0 por eso mandamos 0 con set
+
+
+
+#StringVars que van a ser modificables
 operacion = StringVar()
+resultado = StringVar()
 operacion.set("0")
-
-#label que van a tener nuestra aplicacion 
-
-LabelOperacion = LabelAtributos(frame,operacion,0,3)
-LabelResultado = LabelAtributos(frame,resultado,1,3)
+resultado.set("0")
 
 
 
-#los buttons que van a tener nuestra aplicacion
-button_1 = ButtonAtributos(frame,"1",2,0)
-button_2 = ButtonAtributos(frame,"2",2,1)
-button_3 = ButtonAtributos(frame,"3",2,2)
-button_4 = ButtonAtributos(frame,"4",3,0)
-button_5 = ButtonAtributos(frame,"5",3,1)
-button_6 = ButtonAtributos(frame,"6",3,2)
-button_7 = ButtonAtributos(frame,"7",4,0)
-button_8 = ButtonAtributos(frame,"8",4,1)
-button_9 = ButtonAtributos(frame,"9",4,2)
-button_0 = ButtonAtributos(frame,"0",5,0)
-button_multiplicar = ButtonAtributos(frame,"X",2,3)
-button_Sumar = ButtonAtributos(frame,"+",5,1)
-button_Restar = ButtonAtributos(frame,"-",5,2)
-button_Dividir = ButtonAtributos(frame,"/",3,3)
-button_Porcentaje = ButtonAtributos(frame,"%",4,3)
-button_Resultado = ButtonAtributos(frame,"=",5,3)
+#labels de operacion y el resultado
+
+Labeloperacion = Label(frame,textvariable=operacion)
+Labeloperacion.config(font=("ARIAL", 15))
+Labeloperacion.grid(row=0,column=0,columnspan=4,sticky="e")
+LabelResultaod = Label(frame,textvariable=resultado)
+LabelResultaod.config(font=("ARIAL", 15))
+LabelResultaod.grid(row=1,column=0,columnspan=4,sticky="e")
+
+#Buttons de numeros y operaciones
+
+Boton_7 = Button(frame,text="7",width=10,height=3,command=lambda:CaracteresPantalla("7"))
+Boton_7.grid(row=2,column=0,sticky="NSEW")
+Boton_8 = Button(frame,text="8",width=10,height=3,command=lambda:CaracteresPantalla("8"))
+Boton_8.grid(row=2,column=1,sticky="NSEW")
+Boton_9 = Button(frame,text="9",width=10,height=3,command=lambda:CaracteresPantalla("9"))
+Boton_9.grid(row=2,column=2,sticky="NSEW")
+Boton_10 = Button(frame,text="+",width=10,height=3,command=lambda:CaracteresPantalla("+"))
+Boton_10.grid(row=2,column=3,sticky="NSEW")
+Boton_4 = Button(frame,text="4",width=10,height=3,command=lambda:CaracteresPantalla("4"))
+Boton_4.grid(row=3,column=0,sticky="NSEW")
+Boton_5 = Button(frame,text="5",width=10,height=3,command=lambda:CaracteresPantalla("5"))
+Boton_5.grid(row=3,column=1,sticky="NSEW")
+Boton_6 = Button(frame,text="6",width=10,height=3,command=lambda:CaracteresPantalla("6"))
+Boton_6.grid(row=3,column=2,sticky="NSEW")
+Boton_11 = Button(frame,text="-",width=10,height=3,command=lambda:CaracteresPantalla("-"))
+Boton_11.grid(row=3,column=3,sticky="NSEW")
+Boton_1 = Button(frame,text="1",width=10,height=3,command=lambda:CaracteresPantalla("1"))
+Boton_1.grid(row=4,column=0,sticky="NSEW")
+Boton_2 = Button(frame,text="2",width=10,height=3,command=lambda:CaracteresPantalla("2"))
+Boton_2.grid(row=4,column=1,sticky="NSEW")
+Boton_3 = Button(frame,text="3",width=10,height=3,command=lambda:CaracteresPantalla("3"))
+Boton_3.grid(row=4,column=2,sticky="NSEW")
+Boton_12 = Button(frame,text="x",width=10,height=3,command=lambda:CaracteresPantalla("x"))
+Boton_12.grid(row=4,column=3,sticky="NSEW")
+Boton_Porcentaje = Button(frame,text="%",width=10,height=3,command=lambda:CaracteresPantalla("%"))
+Boton_Porcentaje.grid(row=5,column=0,sticky="NSEW")
+Boton_0 = Button(frame,text="0",width=10,height=3,command=lambda:CaracteresPantalla("0"))
+Boton_0.grid(row=5,column=1,sticky="NSEW")
+Boton_Diviendo = Button(frame,text="/",width=10,height=3,command=lambda:CaracteresPantalla("/"))
+Boton_Diviendo.grid(row=5,column=2,sticky="NSEW")
+Boton_Resultado = Button(frame,text="=",width=10,height=3,command=lambda:CaracteresPantalla("="))
+Boton_Resultado.grid(row=5 ,column=3,sticky="NSEW")
 
 
 
